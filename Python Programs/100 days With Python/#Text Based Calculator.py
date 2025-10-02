1a073ce43711439267792a419a17a3c0b02676c3
agarwalgauarv25 <gagarwal0025@gmail.com>
2025-10-02 17:51:47 +0530

    Create #Text Based Calculator.py
    Hard coded calculator
    dictionary enabled calculator

---
+++ Python Programs/100 days With Python/#Text Based Calculator.py
@@ -0,0 +1,72 @@
+#Text Based Calculator
+import operator
+
+operators ={
+    'add': [operator.add,"+"],
+    'sub': [operator.sub,"-"],
+    'mul': [operator.mul,"*"],
+    'div': [operator.truediv,"/"],
+    "mod" :[operator.mod,"modulous"]
+    }
+
+def calc (first_number,second_number,user_operation):
+    print (f'Your Question is {first_number} {operators[user_operation][1]} {second_number}')
+    if operation in operators:
+            return operators[operation][0](first_number,second_number)
+    #if you wanna see hard coded version open below
+    #region
+    #Hard coding the operations    
+        # if operation == "+":
+        #     result = num1 + num2
+        # if operation == "-":
+        #     result = num1 - num2
+        # if operation == "/":
+        #     result = num1 / num2
+        # if operation == "*":
+        #     result = num1 * num2
+
+        # print(f'{num1} {operation} {num2} = {result}')
+#endregion
+
+    
+
+
+
+should_continue = "Y"
+while should_continue == "Y":
+    num1 = int(input("Enter the first Number: "))
+    num2 = int(input("Enter the second Number: "))
+    print ("List of Operations Available :")
+
+    #Using Loop to print the available operations as per the dictionary operators
+    for i in operators:
+         print(f'👉 {i} =  {num1} {operators[i][1]} {num2}')
+    operation = input("Enter the operation : ")
+    #if you wanna see hard coded version open below
+    #region
+
+
+    # Hard coding the aval operations     
+        # operation = input(
+        #      f'''
+        # List of operations available 
+
+        #      👉 add =  {num1} + {num2} 
+        #      👉 sub =  {num1} - {num2}
+        #      👉 mul = {num1} * {num2}
+        #      👉 div = {num1} / {num2}
+        #      👉 mod = remainder when {num1} is divided by {num2}
+
+        # Enter the operation : ''')
+ #endregion   
+
+    #Printing the user's Question
+    result = calc(num1,num2,operation)
+    print(f'{num1} {(operators[operation][1])} {num2} = {result}')
+
+    #Asking if user wants to continue
+    should_continue = input("Press Y if you wanna continue , press N if you dont\n")
+
+#End the programm once user is done
+if should_continue == "N":
+    print("Thank yuo for using our calculator")
